ac368d1c118eede0f65304c194e7e70393234a82
pedropmedina <pedrodelilia@gmail.com>
2018-05-06 16:16:59 -0400

    Added GET /:id at routes/movies.js

--- routes/movies.js
+++ routes/movies.js
@@ -9,3 +9,14 @@ router.get('/', async (req, res) => {
 	const movies = await Movie.find().sort('name');
 	res.send(movies);
 });
+
+// get movie by id
+router.get('/:id', async (req, res) => {
+	const movie = await Movie.findById(req.params.id);
+
+	if (!movie) {
+		return res.status(404).send('Movie with ID provided not found.');
+	}
+
+	res.send(movie);
+});
