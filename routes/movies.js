2a6d2d3f9102a6a6e2497180dc4335659aba88cb
pedropmedina <pedrodelilia@gmail.com>
2018-05-06 16:37:36 -0400

    Added POST / at routes/movies.js

--- routes/movies.js
+++ routes/movies.js
@@ -20,3 +20,26 @@ router.get('/:id', async (req, res) => {
 
 	res.send(movie);
 });
+
+// post movie
+router.post('/', async (req, res) => {
+	const { error } = validateMovie(req.body);
+	if (error) return res.status(400).send(error.details[0].message);
+
+	const genre = await Genre.findById(req.body.genreId);
+	if (!genre) return res.status(400).send('Invalid genre');
+
+	let movie = new Movie({
+		title: req.body.title,
+		genre: {
+			_id: genre._id,
+			name: genre.name,
+		},
+		numberInStock: req.body.numberInStock,
+		dailyRentalRate: req.body.dailyRentalRate,
+	});
+
+	movie = await movie.save();
+
+	res.send(movie);
+});
