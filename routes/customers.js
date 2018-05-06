d732cb59cefc4206a40616ae68752f5d127bee9d
pedropmedina <pedrodelilia@gmail.com>
2018-05-06 15:31:50 -0400

    Added PUT / at routes/customers

--- routes/customers.js
+++ routes/customers.js
@@ -24,3 +24,25 @@ router.post('/', async (req, res) => {
 
 	res.status(200).send(customer);
 });
+
+// put customers
+router.put('/:id', async (req, res) => {
+	const { error } = validateCustomer(req.body);
+	if (error) return res.status(400).send(error.details[0].message);
+
+	const customer = await Customer.findByIdAndUpdate(
+		req.params.id,
+		{
+			name: req.body.name,
+			isGold: req.body.isGold,
+			phone: req.body.phone,
+		},
+		{ new: true },
+	);
+
+	if (!customer) {
+		return res.status(404).send('Customer with given ID not found');
+	}
+
+	res.send(customer);
+});
