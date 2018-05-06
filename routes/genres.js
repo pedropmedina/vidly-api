153e718e75c7586977c5141254537c791ed1f855
pedropmedina <pedrodelilia@gmail.com>
2018-05-06 15:57:30 -0400

    Added DELETE / at routes/genres.js

--- routes/genres.js
+++ routes/genres.js
@@ -37,3 +37,13 @@ router.put('/:id', async (req, res) => {
 
 	res.send(genre);
 });
+
+router.delete('/:id', async (req, res) => {
+	const genre = await Genre.findByIdAndRemove(req.params.id);
+
+	if (!genre) {
+		return res.status(404).send('Genre with given Id not found.');
+	}
+
+	res.send(genre);
+});
