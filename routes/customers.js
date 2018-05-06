c445ef44dbdd045e6d01fe6b5d488755996a44f9
pedropmedina <pedrodelilia@gmail.com>
2018-05-06 15:24:13 -0400

    Added POST / at routes/customers

--- routes/customers.js
+++ routes/customers.js
@@ -3,7 +3,24 @@ const router = express.Router();
 const { mongoose } = require('../db/mongoose');
 const { Customer, validateCustomer } = require('../models/customer');
 
+// get customers
 router.get('/', async (req, res) => {
 	const customers = await Customer.find().sort('name');
 	res.send(customers);
+});
+
+// post customers
+router.post('/', async (req, res) => {
+	const { error } = validateCustomer(req.body);
+	if (error) return res.status(400).send(error.details[0].message);
+
+	let customer = new Customer({
+		name: req.body.name,
+		isGold: req.body.isGold,
+		phone: req.body.phone,
+	});
+
+	customer = await customer.save();
+
+	res.status(200).send(customer);
 });
