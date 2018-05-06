0b803887638ac7dfe3e97e0e2a4b5dffe55b0e2c
pedropmedina <pedrodelilia@gmail.com>
2018-05-06 15:46:55 -0400

    Added POST / at routes/genres.js

--- routes/genres.js
+++ routes/genres.js
@@ -7,3 +7,16 @@ router.get('/', async (req, res) => {
 	const genres = await Genre.find().sort('name');
 	res.send(genres);
 });
+
+router.post('/', async (req, res) => {
+	const { error } = validateGenre(req.body);
+	if (error) return res.status(400).send(error.details[0].message);
+
+	let genre = new Genre({
+		name: req.body.name,
+	});
+
+	genre = await genre.save();
+
+	res.send(genre);
+});
