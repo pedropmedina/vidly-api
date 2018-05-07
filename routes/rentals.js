c62d8d51f2b88e3fccac473ac16fba3eba82fffa
pedropmedina <pedrodelilia@gmail.com>
2018-05-06 22:55:45 -0400

    Added Fawn

--- routes/rentals.js
+++ routes/rentals.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const Fawn = require('fawn');
 const { mongoose } = require('../db/mongoose');
 const { Rental, validateRental } = require('../models/rental');
 const { Customer } = require('../models/customer');
 const { Movie } = require('../models/movie');
+
+Fawn.init(mongoose);
 
 router.get('/', async (req, res) => {
 	const rentals = await Rental.find().sort('-dateOut');
@@ -36,12 +39,16 @@ router.post('/', async (req, res) => {
 		},
 	});
 
-	rental = await rental.save();
+	try {
+		new Fawn.Task()
+			.save('rentals', rental)
+			.update('movies', { _id: movie._id }, { $inc: { numberInStock: -1 } })
+			.run();
 
-	movie.numberInStock--;
-	movie.save();
-
-	res.send(rental);
+		res.send(rental);
+	} catch (ex) {
+		res.status(500).send('Something failed.');
+	}
 });
 
 router.get('/:id', async (req, res) => {
